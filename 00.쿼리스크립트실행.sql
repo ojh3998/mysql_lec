1d0e27572957f3bcb539a063472c8d08b6c8bce8
김성율 <ojh3998@naver.com>
2024-11-21 16:56:51 +0900

    20241121

---
+++ 00.쿼리스크립트실행.sql
@@ -0,0 +1,13 @@
+-- 작성된 커리문 실행하는 방법
+-- 1. 블록잡고 Ctrl + Enter
+-- 2. 페이지 전체 실행 Ctrl + shift + Enter
+
+-- 스크립트 파일(.sql) 실행하는 방법 (mysql shell 활용)
+-- 1. 명령 프롬프트(cmd) 열기
+-- 2. 사용자 접속하기 
+    -- mysql -u 사용자이름 -p 
+    -- Enter password : **** 
+-- 3. 접속 성공하면 MySQL Shell 로 진입함
+	-- mysql> 
+-- 4. 스크립트 파일 실행 명령 내리기
+    -- mysql> source /path/스크립트파일명.sql    
---
+++ 02.테이블생성및삭제.sql
@@ -0,0 +1,33 @@
+-- 스키마 선택하기
+USE testdb;
+
+/*
+ 테이블은 부모테이블(PK를 가진)을 먼저 만들고, 자식테이블(FK를 가진)을 나중에 만든다.
+ 테이블 삭제와 생성의 순서는 항상 역순으로 작업한다.
+*/
+
+-- 테이블 삭제하기(항상 삭제먼저) 
+DROP TABLE IF EXISTS tbl_order CASCADE;  -- 참조중인 테이블이 존재하면 함께 삭제 - CASCADE    
+DROP TABLE IF EXISTS tbl_product CASCADE;
+
+-- 테이블 만들기
+
+CREATE TABLE IF NOT EXISTS tbl_product
+(
+    prod_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '제품코드',
+    prod_name VARCHAR(20) NULL                                COMMENT '제품이름',            -- NULL 생략가능
+    prod_name VARCHAR(20) NULL                                COMMENT '제품이름',            -- NULL 생략가능
+    price     INT(5)                                          COMMENT '제품가격',            -- INT(자릿수)  
+    stock     SMALLINT    DEFAULT 0                           COMMENT '제품재고'
+) ENGINE=INNODB COMMENT '제품';
+
+CREATE TABLE IF NOT EXISTS tbl_order
+(
+    order_id INT      NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '주문번호',
+    order_user VARCHAR(20)                                COMMENT '주문자',
+    prod_id INT                                           COMMENT '제품번호',
+    order_dt DATETIME DEFAULT NOW()                       COMMENT '주문일자',  -- DEFAULT NOW() -> 오늘
+    FOREIGN KEY(prod_id) REFERENCES tbl_product(prod_id)                       -- 외래키 
+) ENGINE=INNODB COMMENT '주문';
+
+ALTER TABLE tbl_order AUTO_INCREMENT = 1000;                                   -- AUTO_INCREMENT(자동증가) 시작값을 1000으로 바꿈
